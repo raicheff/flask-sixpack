26efd6e7584e6b74d1e93e72eefe0552f74b7758
Boris Raicheff <b@raicheff.com>
2019-01-25 18:10:56 +0000

    Hello, world!

---
+++ setup.py
@@ -0,0 +1,23 @@
+#
+# Flask-Sixpack
+#
+# Copyright (C) 2019 Boris Raicheff
+# All rights reserved
+#
+
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-Sixpack',
+    version='0.1.0',
+    author='Boris Raicheff',
+    author_email='b@raicheff.com',
+    url='https://github.com/raicheff/flask-sixpack',
+    install_requires=['flask', 'sixpack'],
+    py_modules=['flask_sixpack'],
+)
+
+
+# EOF
